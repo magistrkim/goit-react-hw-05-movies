dc024833df291b7b58fa863041fae3f728873f80
Nataliya Kim <magistr1982@gmail.com>
2023-06-01 22:21:53 +0300

    Add basename to BrowserRouter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import MoviesRoutes from '../MoviesRoutes/MoviesRoutes';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <MoviesRoutes />
     </BrowserRouter>
   );
